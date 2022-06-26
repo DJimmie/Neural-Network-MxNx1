92d60221cfa3725ff08d58036ed8c50eaf5e0408
Jim <30752307+DJimmie@users.noreply.github.com>
2022-06-26 11:23:03 -0500

    Updated per GitHub Dependabot alert

---
+++ dependancy_management_script.ps1
@@ -0,0 +1,9 @@
+venv\Scripts\activate
+pip list
+python.exe -m pip install --upgrade pip
+pip install -U ipython
+pip install -U pillow
+pip install -U notebook
+pip install  -U numpy
+pip freeze > requirements.txt
+pip list
